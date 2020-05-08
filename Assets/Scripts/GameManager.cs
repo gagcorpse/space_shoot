76f7b982fa319f542cdffe60a78448c2a6f9c1eb
Benis <gagcorpse@gmail.com>
2020-05-08 22:41:41 +0300

    asd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	//public float barMaxSize, barMinSize, barSize,barSize;
+	public float currentEnergy, maxEnergy, energyCoef;
+	public float energyTimer, energyRegen;
+	public GameObject energyBarProgress;
+	public GameObject energyBarProgressBarSprite;
+	public GameObject energyBarText, generatorCostText;
+	public int currentGeneratorLevel, generatorNextLevelCost;
+	private float energyTimerSet;
+	public Color newColor;
+	public SpriteRenderer pbBarSR;
+	public int money;
+
+	// Use this for initialization
+	void Start () {
+		GeneratorLevelControl ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnergyBar ();
+		EnergyGain ();
+	}
+
+	public void EnergyBar(){
+		energyCoef = currentEnergy / maxEnergy;
+		Vector3 barSizeV = energyBarProgress.GetComponent<Transform> ().localScale;
+		barSizeV.x=energyCoef;
+		energyBarProgress.GetComponent<Transform> ().localScale = barSizeV;
+		energyBarText.GetComponent<TextMesh> ().text = currentEnergy + " / " + maxEnergy;
+
+		Color newColor = new Color ((255-186*energyCoef)/255, (70 + 185*energyCoef)/255, (28 + 58 * energyCoef)/255, 1);
+
+		pbBarSR.color = newColor;
+		//print (newColor);
+	}
+
+	public void GeneratorLevelControl(){
+		if (currentGeneratorLevel == 1) {
+			maxEnergy = 100;
+			energyTimer = 2f;
+			energyTimerSet = 2f;
+			energyRegen = 8f;
+
+			generatorNextLevelCost=100;
+			generatorCostText.GetComponent<TextMesh> ().text = generatorNextLevelCost+"$";
+		}
+		if (currentGeneratorLevel == 2) {
+			maxEnergy = 150;
+			energyTimer = 2f;
+			energyTimerSet = 2f;
+			energyRegen = 12f;
+
+			generatorNextLevelCost=500;
+			generatorCostText.GetComponent<TextMesh> ().text = generatorNextLevelCost+"$";
+		}
+
+		if (currentGeneratorLevel == 3) {
+			maxEnergy = 250;
+			energyTimer = 1.9f;
+			energyTimerSet = 1.9f;
+			energyRegen = 15f;
+
+			generatorNextLevelCost=1500;
+			generatorCostText.GetComponent<TextMesh> ().text = generatorNextLevelCost+"$";
+		}
+
+
+	}
+
+	public void EnergyGain(){
+		energyTimer = energyTimer - Time.deltaTime;
+		if (energyTimer <= 0) {
+			energyTimer = energyTimerSet;
+			currentEnergy = currentEnergy + energyRegen;
+			if(currentEnergy>=maxEnergy)
+			{
+				currentEnergy = maxEnergy;	
+			}
+		}
+	}
+
+	public void GeneratorLevelUp(){
+		if (money >= generatorNextLevelCost && currentGeneratorLevel == 2) {
+			money = money - generatorNextLevelCost;
+			currentGeneratorLevel = 3;
+			GeneratorLevelControl ();
+		}
+
+		if (money >= generatorNextLevelCost && currentGeneratorLevel == 1) {
+			money = money - generatorNextLevelCost;
+			currentGeneratorLevel = 2;
+			GeneratorLevelControl ();
+		}
+	}
+}
---
+++ Assets/Scripts/SortingOrder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingOrder : MonoBehaviour {
+
+	public string sortingLayerName = "Default";
+	public int sortingOrderNumber = 0;
+
+	void Awake ()
+	{
+		gameObject.GetComponent<MeshRenderer> ().sortingLayerName = sortingLayerName;
+		gameObject.GetComponent<MeshRenderer> ().sortingOrder = sortingOrderNumber;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
